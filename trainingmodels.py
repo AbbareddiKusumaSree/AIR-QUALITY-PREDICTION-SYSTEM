f0091019577c1d6421a5af446a0ed806be405bfc
AbbareddiKusumaSree <kusumaabbareddi@gmail.com>
2025-09-17 14:41:08 +0530

    Initial commit

--- trainingmodels.py
+++ trainingmodels.py
@@ -1,9 +1,7 @@
 # milestone2.py
 # Train & Evaluate Models (ARIMA, Prophet, LSTM)
-
-# -----------------------------
 # Import Libraries
-# -----------------------------
+
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error
